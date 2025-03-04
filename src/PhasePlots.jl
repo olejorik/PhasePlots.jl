bfa79c1e0a78113da33ebc5fef37711b7203c5d0
Oleg Soloviev <oleg.soloviev@gmail.com>
2025-03-04 14:38:11 +0100

    Add dpng and save_figs_as_gif functions to fileutils.jl

--- src/PhasePlots.jl
+++ src/PhasePlots.jl
@@ -8,4 +8,8 @@ include("fileutils.jl")
 include("ellipse.jl")
 include("phasedisplay.jl")
 
+
+
+export dpng, save_figs_as_gif
+
 end
--- src/fileutils.jl
+++ src/fileutils.jl
@@ -2,22 +2,35 @@
 """
     dpng(x)
 
-Displays `x` as PNG. Can be used when the default representation 
+Displays `x` as PNG. Can be used when the default representation
 (SVG or PDF) is too heavy.
 """
-dpng(x) = display("image/png",x)
+dpng(x) = display("image/png", x)
 
 struct save2
     name::String
     subfolder::String
 end
 
-save2(name) = save2(name,"notebooks/phase_from_interferograms/images/")
+save2(name) = save2(name, "notebooks/phase_from_interferograms/images/")
 
 
 # (s::save2)(fig) = (wsave("$(s.subfolder)$(s.name).pdf", fig); wsave("$(s.subfolder)$(s.name).png", fig);)
 
 
-export dpng
 
 
+function save_figs_as_gif(figs, name, fps=2)
+    if @isdefined Makie
+        cb = Makie.current_backend()
+    else
+        error("No Makie found")
+    end
+    GLMakie.activate!()
+    figanim = Figure(; size=figs[1].scene.camera.resolution[])
+    record(figanim, name, figs; framerate=fps) do f
+        figanim = f
+        display(figanim)
+    end
+    return cb.activate!()
+end
